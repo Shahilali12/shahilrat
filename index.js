c3367905b0f7f3e7a8f48e91b4aa4a496705a575
Shahilali12 <89555969+Shahilali12@users.noreply.github.com>
2024-04-15 19:23:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 6889494690:AAE9OBtRHoB_iKm514O3f5SijiORFR06k8I'
-const id = '7076342781'
+const token = 6871565224:AAGbyf-Yj5pa0xJLGQTz8mZsHjluplW3obc'
+const id = '6517101752'
 const address = 'https://www.google.com'
 
 const app = express();
